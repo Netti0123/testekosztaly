08d06e516b10d3996539f2af497b206e1d8e40d0
Netti0123 <113418004+Netti0123@users.noreply.github.com>
2023-09-26 10:51:06 +0200

    Add files via upload

---
+++ testek_osztalya/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testek_osztalya
+{
+    abstract class Test
+    {
+        public string tipus;
+
+        protected Test(string tipus) 
+        { 
+            this.tipus = tipus;
+        }
+        public abstract double Terfogat();
+        public abstract double Felszin();
+        public override string ToString()
+        {
+            return tipus;
+        }
+
+    }
+}
---
+++ testek_osztalya/Téglatest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testek_osztalya
+{
+    internal class Téglatest : Test
+    {
+        double oldalA;
+        double oldalB;
+        double oldalC;
+        public Téglatest(string tipus) : base(tipus)
+        {   
+        }
+        public double OldalA { get => oldalA; }
+        public double OldalB { get => oldalB; }
+        public double OldalC { get => oldalC; }
+
+        public override double Felszin()
+        {
+
+            return 2 * ((oldalA * oldalB) + (oldalA * oldalC) + (oldalB * oldalC));
+        
+        }
+
+        public override double Terfogat()
+        {
+             return oldalA * oldalB * oldalC;
+        }
+    }
+}
